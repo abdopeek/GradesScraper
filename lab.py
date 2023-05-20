fb6830d2a2af863a24a7f0be3282a997ebba95f9
Abdulrahem Mahgoub <abdulrahimmahgoub@gmail.com>
2023-05-20 13:48:36 +0300

    Update lab.py
    Automated process of finding the sectionId's and hardcoding them, now all you have to do to get the grades is simply login

--- lab.py
+++ lab.py
@@ -1,24 +1,27 @@
+import re
+import pwinput
+from utils import *
 from selenium import webdriver  # use it to open the actual browser for cookies
-from selenium.webdriver.edge.service import Service  
+from selenium.webdriver.edge.service import Service
 from selenium.webdriver.common.keys import Keys  # to use a keyboard
 from selenium.webdriver.common.by import By  # to locate element
 import requests  # to work with requests instead of selenium
-from utils import head  # header variable
 import json  # work with the return from the website
 from time import sleep  # sleep
 
-
-ser = Service(Driver_location)
+ser = Service(DRIVER_LOCATION)
 op = webdriver.EdgeOptions()
 op.add_argument('headless')
 op.add_experimental_option('excludeSwitches', ['enable-logging'])
 # op.add_experimental_option('detach', True)
 driver = webdriver.Edge(service=ser, options=op)
 driver.minimize_window()
-driver.get("https://upeisis.uofcanada.edu.eg/PowerCampusSelfService/Grades/GradeReport")
+driver.get("https://upeisis.uofcanada.edu.eg/PowerCampusSelfService/Registration/Schedule")
+s = requests.Session()
 sleep(2)
 
-def set_cookies(s, cookies):
+
+def set_cookies(cookies, header):
     default = ['messagesUtk=50c6732c976448b3abbeadcea96c0180', '', '']
     for cookie in cookies:
         updated = f"{cookie['name']}={cookie['value']}"
@@ -26,9 +29,7 @@ def set_cookies(s, cookies):
             default[2] = updated
         elif cookie['name'] == "ASP.NET_SessionId":
             default[1] = updated
-    head['cookie'] = ';'.join(default)
-
-
+        header['cookie'] = ';'.join(default)
 
 
 def enterUsername(username):
@@ -44,27 +45,67 @@ def enterPassword(password):
     search.send_keys(Keys.RETURN)
 
 
-def get_data():
+def get_student_id():
+    url = 'https://upeisis.uofcanada.edu.eg/PowerCampusSelfService/Registration/Schedule'
+    sleep(1.5)
+    cookies = driver.get_cookies()
+    set_cookies(cookies, section_head)
+    resp = s.get(url, headers=section_head)
+    pattern = r'<input\s+id="hdnPersonId"\s+type="hidden"\s+value="(\d+)"\s*/>'
+    match = re.search(pattern, resp.text)
+    if match:
+        value = match.group(1)
+        return value
+    else:
+        raise "ERROR"
+
+
+def get_sections(id):
+    sections = []
+    url = r'https://upeisis.uofcanada.edu.eg/PowerCampusSelfService/Schedule/Student'
+    session = {"year": "2023", "term": "WINTER", "session": ""}  # update this every semester
+    payload = {'personId': id, "yearTermSession": session}
+    sleep(1.5)
+    cookies = driver.get_cookies()
+    set_cookies(cookies, sched_head)
+    resp = s.post(url, data=json.dumps(payload), headers=sched_head)
+    if resp.status_code == 200:
+        resp = resp.json()
+    else:
+        raise "ERROR"
+    data = json.loads(resp)['data']['schedule'][0]['sections']
+    for i in data:
+        if i:
+            data = i
+    for section in data:
+        if section['eventSubType'] == "Lecture":
+            sections.append(section['id'])
+    return sections
+
+
+def get_data(param):
     grade_link = 'https://upeisis.uofcanada.edu.eg/PowerCampusSelfService/Students/ActivityGrades'
 
-    param = [{"sectionId": "4965"}, {"sectionId": "5120"}, {"sectionId": "5014"}, {"sectionId": "5034"}]  # hard code course id's
-    s = requests.Session()
     sleep(1.5)  # wait for cookies to load in
     cookies = driver.get_cookies()
-    set_cookies(s, cookies)
+    set_cookies(cookies, grades_head)
     outputs = []
     for section in param:
-        resp = s.post(grade_link, data=json.dumps(section), headers=head)
+        resp = s.post(grade_link, data=json.dumps(section), headers=grades_head)
         if resp.status_code == 200:  # accepted
             resp = resp.json()
         else:
             return "Error"
-        json_value = json.loads(resp)['data']
-        output = {
-            'name': json_value['sectionName'],
-            'data': json_value
-        }
-        outputs.append(output)
+        try:
+            json_value = json.loads(resp)['data']
+            output = {
+                'name': json_value['sectionName'],
+                'data': json_value
+            }
+        except:
+            pass
+        else:
+            outputs.append(output)
 
     return outputs
 
@@ -105,6 +146,7 @@ def print_output(data):
     name = data['name']
     scores = data['data']
     assignments = scores['finalTermAssignments']
+    print("-------------------------------------------")
     print(f"\t\t{name}\t\t")
     for assignment in assignments:
         print(f"{assignment['description']}")
@@ -120,13 +162,17 @@ def print_output(data):
     print("-------------------------------------------")
     print("-------------------------------------------")
 
-
 def main():
-    enterUsername(USERNAME)
-    enterPassword(PASSWORD)
-    jsons = get_data()
+    username = input("Username: ")
+    password = pwinput.pwinput('Password: ', mask='*')
+    enterUsername(username)
+    enterPassword(password)
+    student = get_student_id()
+    sections = get_sections(student)
+    sections = [{'sectionId': f"{id}"} for id in sections]
+    jsons = get_data(sections)
     if jsons == "Error":
-        return "Error"
+        return "Error occurred"
     for json_data in jsons:
         processed = process(json_data['data'])
         json_data['data'] = processed
