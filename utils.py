9ac40cd0d5fea73e97d4982d856d417439b61b92
Abdulrahem Mahgoub <abdulrahimmahgoub@gmail.com>
2023-05-20 13:50:03 +0300

    Update utils.py
    New headers for updated code

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-head = {
+grades_head = {
     'accept': 'application/json',
     'accept-encoding': 'gzip, deflate, br',
     'accept-language': 'en-US,en;q=0.9,ar;q=0.8',
@@ -15,3 +15,44 @@ head = {
     'sec-fetch-site': 'same-origin',
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'
 }
+
+section_head = {
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+    "accept-encoding": "gzip, deflate, br",
+    "accept-language": "en-US,en;q=0.6",
+    "cache-control": "max-age=0",
+    "referer": "https://upeisis.uofcanada.edu.eg/PowerCampusSelfService",
+    "sec-ch-ua": '"Brave";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"Windows"',
+    "sec-fetch-dest": "document",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-site": "same-origin",
+    "sec-fetch-user": "?1",
+    "sec-gpc": "1",
+    "upgrade-insecure-requests": "1",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36"
+}
+
+sched_head = {
+    'accept': 'application/json',
+    'accept-encoding': 'gzip, deflate, br',
+    'accept-language': 'en-US,en;q=0.6',
+    'cache-control': 'max-age=0',
+    'content-length': '80',
+    'content-type': 'application/json',
+    'origin': 'https://upeisis.uofcanada.edu.eg',
+    'referer': 'https://upeisis.uofcanada.edu.eg/PowerCampusSelfService/Registration/Schedule',
+    'sec-ch-ua': '"Brave";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-origin',
+    'sec-gpc': '1',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'
+}
+
+# grades_head is to access the grades section
+# section_head is to find the student_id to use in finding sections
+# sched_head is to find section_id to use in finding grades
